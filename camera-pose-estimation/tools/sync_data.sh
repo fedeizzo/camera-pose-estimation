88399b481316fa9aeeedbbd7db0269238010dd41
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2021-10-30 11:52:23 +0200

    Update sync data to drive

--- camera-pose-estimation/tools/sync_data.sh
+++ camera-pose-estimation/tools/sync_data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 project_root=$(git rev-parse --show-toplevel)
-rclone sync ${project_root}/data drive-uni:SIV/data --progress
+rclone copy ${project_root}/data drive-uni:SIV/data --progress
+rclone copy drive-uni:SIV/data ${project_root}/data --progress
