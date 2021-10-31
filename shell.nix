0bbf1214e0d5a00e756e221ad7699e1231afcdc2
Federico Izzo <federico.izzo99@gmail.com>
2021-10-31 19:11:39 +0100

    Add aim, update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,34 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
-
 let
-  mach-nix = import (builtins.fetchGit {
-    url = "https://github.com/DavHau/mach-nix/";
-    ref = "refs/heads/master";
-    rev = "dc94135e31d5c90c40a00a6cbdf9330526e8153b";
-  }) {
-    python = "python39";
-    pypiDataRev = "523d0a516d4ba8d999f507838e9b7a5db476437c";
-    pypiDataSha256 = "0w1l53wccspwix97v42yzp3nhf73l0vydvwyz5x12vfsaikwvpy6";
-  };
-  pythonEnv = mach-nix.mkPython rec {
-    requirements = builtins.readFile ./requirements.txt;
-    providers = {
-      _defualt = "wheel";
-    };
-    packagesExtra = [
-      ./config_parser
-    ];
-  };
+  shell = builtins.getEnv "SHELL";
 in
-mach-nix.nixpkgs.mkShell {
-  buildInputs = [
+(pkgs.buildFHSUserEnv {
+  name = "camera-pose-estimation";
+  targetPkgs = pkgs: (with pkgs; [
     pkgs.ffmpeg
     pkgs.colmapWithCuda
-    pkgs.python39Packages.pytorch-bin
     pkgs.meshlab
-    pythonEnv
-  ];
-  shellHook = ''
-    export PATH=$PATH:$(pwd)/camera-pose-estimation/tools
-  '';
-}
+    pkgs.python39
+    pkgs.python39Packages.pip
+    pkgs.python39Packages.setuptools
+  ]);
+  runScript = shell;
+}).env
