e99addfbf142641ecb306b47458d568bc7a08d83
Federico Izzo <federico.izzo99@gmail.com>
2021-10-31 19:12:34 +0100

    Add papers directory to sync tool

--- camera-pose-estimation/tools/sync_data.sh
+++ camera-pose-estimation/tools/sync_data.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 project_root=$(git rev-parse --show-toplevel)
 rclone copy ${project_root}/data drive-uni:SIV/data --progress
+rclone copy ${project_root}/papers drive-uni:SIV/papers --progress
 rclone copy drive-uni:SIV/data ${project_root}/data --progress
+rclone copy drive-uni:SIV/papers ${project_root}/papers --progress
